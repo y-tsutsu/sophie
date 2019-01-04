c1d88f035c55fd13d972626c05b2beb7cec688a8
tsutsu <tsutsu0928@gmail.com>
2019-01-05 01:01:00 +0900

    複数メモの編集に対応．

--- src/components/Editor.vue
+++ src/components/Editor.vue
@@ -5,7 +5,27 @@
       <span>{{ user.displayName }}</span>
       <v-btn color="warning" class="login-btn" @click="logout">ログアウト</v-btn>
       <div class="editorWrapper">
-        <v-textarea outline rows="40" label="Markdown" class="markdown" v-model="markdown"></v-textarea>
+        <div class="memoListWrapper">
+          <v-list class="memoList">
+            <v-list-tile ripple v-for="(memo, index) in memos" :key="index" @click="selectMemo(index)">
+              <v-list-tile-action>
+                <v-icon v-if="index == selectedIndex" color="pink">check_box</v-icon>
+                <v-icon v-else color="pink">check_box_outline_blank</v-icon>
+              </v-list-tile-action>
+              <v-list-tile-content>
+                <v-list-tile-title v-text="displayTitle(memo.markdown)"></v-list-tile-title>
+              </v-list-tile-content>
+            </v-list-tile>
+          </v-list>
+          <v-btn color="info" class="addMemoBtn" @click="addMemo">Add Memo</v-btn>
+        </div>
+        <v-textarea
+          outline
+          rows="40"
+          label="Markdown"
+          class="markdown"
+          v-model="memos[selectedIndex].markdown"
+        ></v-textarea>
         <div class="preview" v-html="preview()"></div>
       </div>
     </div>
@@ -19,7 +39,12 @@ export default {
   props: ["user"],
   data() {
     return {
-      markdown: ""
+      memos: [
+        {
+          markdown: "Untitled Memo"
+        }
+      ],
+      selectedIndex: 0
     };
   },
   methods: {
@@ -27,8 +52,17 @@ export default {
       // eslint-disable-next-line no-undef
       firebase.auth().signOut();
     },
+    addMemo: function() {
+      this.memos.push({ markdown: "Untitled Memo" });
+    },
+    selectMemo: function(index) {
+      this.selectedIndex = index;
+    },
     preview: function() {
-      return marked(this.markdown);
+      return marked(this.memos[this.selectedIndex].markdown);
+    },
+    displayTitle: function(text) {
+      return text.split(/\n/)[0];
     }
   }
 };
@@ -40,6 +74,13 @@ export default {
 }
 .editorWrapper > * {
   padding: 1rem;
+}
+.memoListWrapper {
+  width: 20%;
+}
+.memoTitle {
+  white-space: nowrap;
+  overflow: hidden;
 }
 .markdown {
   width: 50%;
